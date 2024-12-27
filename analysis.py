a4ed151021ff9568b7620389ea0c2dbff5b58958
Zanehisako <yassinetakiko@gmail.com>
2024-12-27 23:28:40 +0100

    started the analysis

--- analysis.py
+++ analysis.py
@@ -1,1 +1,6 @@
-import mat
+import matplotlib.pyplot as plt
+import pandas as pd
+
+global_stats_df = pd.read_csv("Champion_stats.csv")
+plt.bar(global_stats_df['Champion'],global_stats_df['Games Played'])
+plt.show()
